a5fcbafdd0b93b926d4daddf7d0cb4cbc15f23a4
Nagi <nagiitoshi@gmail.com>
2025-02-03 19:47:47 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Footer } from './components/Footer';
-import { Contact } from '../api/Contact';
+import { Contact } from './api/Contact';
 
 function App() {
   return (
